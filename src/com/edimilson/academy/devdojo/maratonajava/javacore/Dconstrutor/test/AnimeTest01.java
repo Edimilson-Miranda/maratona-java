b82a0167b93f124f0ec875476ada3ac9bb615542
Edimilson-Miranda <edimilsonsmp.464@gmail.com>
2026-05-17 10:56:50 -0300

    Feat: Iniciando os estudos em construtor e sobrecarga do mesmo

---
+++ src/com/edimilson/academy/devdojo/maratonajava/javacore/Dconstrutor/test/AnimeTest01.java
@@ -0,0 +1,22 @@
+package com.edimilson.academy.devdojo.maratonajava.javacore.Dconstrutor.test;
+
+
+import com.edimilson.academy.devdojo.maratonajava.javacore.Dconstrutor.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("xxxHOLIC", "TV", 24, "Sobrenatural");
+        Anime anime2 = new Anime();
+
+    //    anime.init("Bleach", "TV", 24);
+
+    //    anime.init("Bleach", "TV", 24, "Açao");
+
+    //    anime.setNome("Bleach");
+    //    anime.setTipo("TV");
+    //    anime.setEpisodio(24);
+
+        anime.imprime();
+        anime2.imprime();
+    }
+}
